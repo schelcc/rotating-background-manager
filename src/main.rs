fea239addc3a0b9ba5f6ad1b3422874ded4b2150
Cole Scheller <schelcc@pobox.com>
2023-05-24 15:51:43 -0400

    rand updates

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,71 @@
+// mod img_tools;
+
 use std::fs;
 use std::fs::File;
 use std::path::Path;
 
+use rand::seq::SliceRandom;
 use rusqlite::{params, Connection, Result, named_params};
 
 fn main() -> Result<()> {
     let db_path = "/home/schelcc/.wallpapers/backgrounds.db";
     let bg_path = "/home/schelcc/.wallpapers/unsorted/";
 
-    // let db = Connection::open(db_path)?;
+    let db = Connection::open(db_path)?;
 
-    refresh_background_db(&db_path, &bg_path)?;
+    refresh_background_db(&db, &bg_path)?;
+
+    // match select_true_random(&db) {
+    //     Err(_) => (),
+    //     Ok(res) => println!("Selected: {}", res)
+    // };
 
     Ok(())
 }
 
+fn select_true_random(db:&Connection) -> Result<String, ()> {
+    let mut stmt = match db.prepare("SELECT backgrounds.path FROM backgrounds") {
+        Err(why) => {
+            println!("[ERR] Selection statement preparation: {:?}", why);
+            return Err(())
+        },
+        Ok(res) => res
+    };
+
+    let results = match stmt.query_map([], |r| r.get::<usize, String>(0)) {
+        Err(why) => {
+            println!("[ERR] Query map: {:?}", why);
+            return Err(())
+        },
+        Ok(res) => res
+    };
+
+    let mut selection_vec : Vec<String> = Vec::new();
+
+    for res in results {
+        selection_vec.push(match res {
+            Err(why) => {
+                println!("[ERR] Vector construction: {:?}", why);
+                return Err(())
+            },
+            Ok(res) => res
+        });
+    };
+
+    let selection : String = match selection_vec.choose(&mut rand::thread_rng()) {
+        None => {
+            println!("[ERR] Random selection: No result from random selection");
+            return Err(())
+        },
+        Some(sel) => sel.to_string()
+    };
+
+    Ok(selection)
+}
+
 
 // Refresh backgrounds database, adding entries for any new images
-fn refresh_background_db(db_path:&str, bg_path:&str) -> Result<()> {
-    let db = Connection::open(db_path)?;
-
+fn refresh_background_db(db:&Connection, bg_path:&str) -> Result<()> {
     // Create auxiliary table for matching
     db.execute("CREATE TABLE auxiliary (path TEXT)", ())?;
 
