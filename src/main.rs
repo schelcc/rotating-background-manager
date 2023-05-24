a4dd16084cf7c9232f4a54976f6d7f6d4e51f428
Cole Scheller <schelcc@pobox.com>
2023-05-24 15:56:37 -0400

    simplified file refresh

--- src/main.rs
+++ src/main.rs
@@ -66,48 +66,27 @@ fn select_true_random(db:&Connection) -> Result<String, ()> {
 
 // Refresh backgrounds database, adding entries for any new images
 fn refresh_background_db(db:&Connection, bg_path:&str) -> Result<()> {
-    // Create auxiliary table for matching
-    db.execute("CREATE TABLE auxiliary (path TEXT)", ())?;
-
-    let insertion = "INSERT INTO auxiliary VALUES (?1)";
-    let mut auxiliary_insertion_stmt = db.prepare(insertion)?;
 
     let mut insertion_count : u64 = 0;
+
+    let mut update = db.prepare("INSERT INTO backgrounds VALUES (?1, 0)")?;
 
     // Read bg_path for all files (TODO: Glob for just image types)
     match fs::read_dir(bg_path) {
         Err(why) => println!("Error: {:?}", why.kind()),
         Ok(paths) => for path in paths {
-            auxiliary_insertion_stmt.execute( 
-                    params![
-                        path.as_ref()
-                        .unwrap()
-                        .path()
-                        .display()
-                        .to_string()])?;
+            let path_str = path.as_ref()
+            .unwrap()
+            .path()
+            .display()
+            .to_string();
+
+            match update.execute(params![path_str]) {
+                Err(_) => (),
+                Ok(_) => {insertion_count += 1;}
+            };
         }
     }
-
-    // Query all rows in aux and not in backgrounds, then update backgrounds accordingly
-    let query = "SELECT auxiliary.path FROM auxiliary WHERE auxiliary.path NOT IN (SELECT backgrounds.path FROM backgrounds)";
-    let mut query_stmt = db.prepare(query)?;
-
-    // Prepare an insertion statement for query results
-    let insertion = "INSERT INTO backgrounds VALUES (?1, ?2)";
-    let mut insertion_stmt = db.prepare(insertion)?;
-
-    // Retrieve the path entry
-    let results = query_stmt.query_map([], |r| r.get::<usize, String>(0))?;
-
-    // We specifically loop rather than map here because for the compiler to actually execute the query,
-    // the resulting iterator must be consumed
-    for item in results {
-        insertion_stmt.execute(params![item?, 0u64])?;
-        insertion_count += 1;
-    };
-
-    // Drop the auxiliary table
-    db.execute("DROP TABLE auxiliary", ())?;
 
     println!("[INFO] Backgrounds database refreshed - {} new entries added", insertion_count);
 
